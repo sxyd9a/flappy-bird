89541ec3325aef21141455c171a43ce06361d512
sxyd9a <syed.abbas14864@gmail.com>
2025-05-12 18:25:20 -0400

    added pipe logic and looped upper pipe segment on each frame

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -27,12 +27,31 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
         }
     }
 
+    //Pipes
+    int pipeX = boardWidth, pipeY = 0;
+    int pipeWidth = 64, pipeHeight = 512;
+
+    class Pipe {
+        int x = pipeX, y = pipeY;
+        int width = pipeWidth, height = pipeHeight; 
+        Image img;
+        boolean passed = false; //check if bird has passed pipe
+
+        Pipe(Image img) {
+            this.img = img;
+        }
+    }
+
     //game logic
     Bird bird;
-    int velocityY = 0;
+    int velocityX = -4; //rate at which pipes move left
+    int velocityY = 0; //rate at which bird moves up
     int gravity = 1;
 
+    ArrayList<Pipe> pipes;
+
     Timer gameLoop;
+    Timer placePipesTimer;
 
     FlappyBird() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
@@ -48,10 +67,25 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
 
         //bird
         bird = new Bird(birdImg);
+        pipes = new ArrayList<Pipe>();
+
+        //place pipes timer
+        placePipesTimer = new Timer(1500, new ActionListener() { //place pipes every 1500 ms
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                placePipes();
+            }
+        });
+        placePipesTimer.start();
 
         //game timer
         gameLoop = new Timer(1000/60, this); //60 fps
         gameLoop.start();
+    }
+
+    public void placePipes() {
+        Pipe topPipe = new Pipe(topPipeImg);
+        pipes.add(topPipe); //add a new pipe every 1500 ms
     }
 
     public void paintComponent(Graphics g) {
@@ -65,18 +99,30 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
 
         //bird
         g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
+
+        //pipes
+        for(int i = 0; i<pipes.size(); i++){
+            Pipe pipe = pipes.get(i);
+            g.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height, null);
+        } 
     }
 
     public void move(){
         //bird
         velocityY += gravity; //account for downward force at every motion
-        bird.y += velocityY; 
+        bird.y += velocityY; //move upwards at a rate of -9 pixels/frame
         bird.y = Math.max(bird.y, 0); //ensure bird position can only ever be current position or top of screen
+
+        //pipes
+        for(int i = 0; i<pipes.size(); i++){
+            Pipe pipe = pipes.get(i);
+            pipe.x += velocityX; //every frame, move each pipe -4 left
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        move(); //move upwards at a rate of -6 pixels/frame
+        move(); 
         repaint();
     }
 
